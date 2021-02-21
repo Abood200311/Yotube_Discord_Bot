9d223c339b6b273fdf9e95509b2e6a4d4685d192
Abood200311 <61956141+Abood200311@users.noreply.github.com>
2021-02-20 19:59:45 -0500

    Add files via upload

--- YT_Bot/src/main/java/Default/App.java
+++ YT_Bot/src/main/java/Default/App.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class App {
     public static void main(String[] args) throws LoginException {
 
-        String token = "ODEyODA5MDI2MTIyOTQwNDU2.YDGJZQ.TmFcROcRlRaw75H26oQbIt7Wg80";
+        String token = "token goes here!";
 
         JDABuilder.createDefault(token)
                   .addEventListeners(new ClearCommand())
